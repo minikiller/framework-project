4db6cfc72975c55add246ccffcf97f92402a116d
majian-rexen <mj7612@163.com>
2015-07-01 14:55:15 +0800

    add core web module

---
+++ modules.core/core-web/src/main/java/cn/com/rexen/core/web/internal/InitActivator.java
@@ -0,0 +1,31 @@
+package cn.com.rexen.core.web.internal;
+
+import org.apache.log4j.Logger;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.http.HttpService;
+
+public class InitActivator implements BundleActivator {
+
+    private static BundleContext context;
+    private static Logger logger = Logger.getLogger(InitActivator.class);
+    private ServiceReference reference;
+
+    @Override
+    public void start(BundleContext bundleContext) throws Exception {
+        logger.info("Start-up core web resources bundle!!");
+        context = bundleContext;
+        reference = bundleContext.getServiceReference(HttpService.class.getName());
+        HttpService httpService = (HttpService) bundleContext.getService(reference);
+        httpService.registerResources("/core-web222/ext-5.1.0", "/webapp/ext-5.1.0", null);
+    }
+
+    @Override
+    public void stop(BundleContext bundleContext) throws Exception {
+        logger.info("Stop core web resources bundle!!");
+        if (reference != null)
+            bundleContext.ungetService(reference);
+        context = null;
+    }
+}
