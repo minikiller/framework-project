5a9379fd0156f5232343bc31f83555572b408f1d
majian-rexen <mj7612@163.com>
2015-07-06 17:24:35 +0800

    重构 core-web

--- modules.admin/admin-web/src/main/java/cn/com/rexen/admin/web/internal/InitActivator.java
+++ modules.admin/admin-web/src/main/java/cn/com/rexen/admin/web/internal/InitActivator.java
@@ -4,6 +4,8 @@ import cn.com.rexen.core.util.SystemUtil;
 import org.apache.log4j.Logger;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.http.HttpService;
 
 /**
  * Created by sunlf on 14-3-23.
@@ -13,16 +15,24 @@ public class InitActivator implements BundleActivator {
     private static final String BUNDLE_NAME = " Kalix Admin Web ";
     private static BundleContext context;
     private static Logger logger = Logger.getLogger(InitActivator.class);
+    private ServiceReference reference;
 
     @Override
     public void start(BundleContext bundleContext) throws Exception {
         SystemUtil.succeedPrintln(String.format("Start-up %s bundle!!", BUNDLE_NAME));
         context = bundleContext;
+
+        reference = bundleContext.getServiceReference(HttpService.class.getName());
+        HttpService httpService = (HttpService) bundleContext.getService(reference);
+        httpService.registerResources("/kalix/app/admin", "/webapp/admin", null);
     }
 
     @Override
     public void stop(BundleContext bundleContext) throws Exception {
         SystemUtil.succeedPrintln(String.format("Stop %s bundle!!", BUNDLE_NAME));
+
+        if (reference != null)
+            bundleContext.ungetService(reference);
         context = null;
     }
 }
