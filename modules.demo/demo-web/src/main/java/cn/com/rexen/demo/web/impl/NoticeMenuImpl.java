a2717bb65a116fe9739d270b0312a77f97b4293b
majian-rexen <mj7612@163.com>
2015-08-12 16:35:22 +0800

    应用模块 实现动态授权

--- modules.demo/demo-web/src/main/java/cn/com/rexen/demo/web/impl/NoticeMenuImpl.java
+++ modules.demo/demo-web/src/main/java/cn/com/rexen/demo/web/impl/NoticeMenuImpl.java
@@ -53,4 +53,9 @@ public class NoticeMenuImpl implements IMenu {
     public int getIndex() {
         return 0;
     }
+
+    @Override
+    public String getPermission() {
+        return null;
+    }
 }
