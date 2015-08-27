11bf5ca39637fbfc6013362868413ce01c87e1a0
sunlf <hanbi918@163.com>
2015-08-27 22:23:37 +0800

    add javers test

--- modules.example/example-mongodb/README.md
+++ modules.example/example-mongodb/README.md
@@ -1,1 +1,2 @@
 1.testing mongodb
+2.testing javers
---
+++ modules.example/example-mongodb/src/main/java/cn/com/rexen/example/mongodb/Matcher.java
@@ -0,0 +1,22 @@
+package cn.com.rexen.example.mongodb;
+
+import org.javers.core.Javers;
+import org.javers.core.JaversBuilder;
+import org.javers.core.diff.Diff;
+
+/**
+ * Created by rmaia on 2/11/15.
+ */
+public class Matcher {
+
+    public static boolean Match(Object base, Object working) {
+
+        Javers javers = JaversBuilder.javers().build();
+
+        Diff compare = javers.compare(base, working);
+//        javers.commit()
+        System.out.println(compare.prettyPrint());
+        return !compare.hasChanges();
+
+    }
+}
