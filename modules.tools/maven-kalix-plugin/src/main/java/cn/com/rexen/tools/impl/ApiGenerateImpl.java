40b4fea54cdb5206e1546f7184e314799541cf1f
sunlf <hanbi918@163.com>
2015-11-05 22:29:18 +0800

    修改代码自动生成模板,支持查询功能的代码生成

--- modules.tools/maven-kalix-plugin/src/main/java/cn/com/rexen/tools/impl/ApiGenerateImpl.java
+++ modules.tools/maven-kalix-plugin/src/main/java/cn/com/rexen/tools/impl/ApiGenerateImpl.java
@@ -12,6 +12,7 @@ public class ApiGenerateImpl extends AbstractGenernateImpl {
     //需要替换名字的java类名
     String serviceFileName = "I%sBeanService.java";
     String daoFileName = "I%sBeanDao.java";
+    String dtoFileName = "%sDTO.java";
     String activatorFileName = "InitActivator.java";
 
     public ApiGenerateImpl(Map<String, String> attributes, File inputDir, File outputDir) {
@@ -19,6 +20,7 @@ public class ApiGenerateImpl extends AbstractGenernateImpl {
         fileMap.put("IBeanService", "//" + moduleName + "//biz//" + String.format(serviceFileName, beanName));
         fileMap.put("IBeanDao", "//" + moduleName + "//dao//" + String.format(daoFileName, beanName));
         fileMap.put("InitActivator", "//" + moduleName + "//internal//" + activatorFileName);
+        fileMap.put("DTO", "//" + moduleName + "//query//" + String.format(dtoFileName, beanName));
     }
 
     @Override
