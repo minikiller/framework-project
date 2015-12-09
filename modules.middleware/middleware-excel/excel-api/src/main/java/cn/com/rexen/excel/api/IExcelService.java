95a91a72dc3e9d244923a2350b00364d6ffddab2
chenyanxu-rexen <chenyanxu@rexen.com.cn>
2015-12-09 17:41:00 +0800

    excel impl

--- modules.middleware/middleware-excel/excel-api/src/main/java/cn/com/rexen/excel/api/IExcelService.java
+++ modules.middleware/middleware-excel/excel-api/src/main/java/cn/com/rexen/excel/api/IExcelService.java
@@ -1,9 +1,12 @@
 package cn.com.rexen.excel.api;
 
 import java.io.InputStream;
+import java.util.Dictionary;
+import java.util.List;
+import java.util.Map;
 
 /**
- * Created by lenovo on 2015/12/7.
+ * @author chenyanxu
  */
 public interface IExcelService {
     Object OpenExcel(String excelPath);
@@ -13,4 +16,10 @@ public interface IExcelService {
     Object OpenSheet(Object wb, String sheetName);
 
     int GetRowCount(Object sheet);
+
+    List<String> GetColumnNames(Object sheet, int columnRowIndex);
+
+    Map<String, Integer> GetColumnDic(Object sheet, int columnRowIndex, Dictionary<String, Object> dic);
+
+    Map<String, Object> GetRowMap(Object sheet, int rowIndex, Dictionary<String, Integer> dic);
 }
--- modules.middleware/middleware-excel/excel-core/src/main/java/cn/com/rexen/excel/core/internal/InitActivator.java
+++ modules.middleware/middleware-excel/excel-core/src/main/java/cn/com/rexen/excel/core/internal/InitActivator.java
@@ -1,7 +1,15 @@
 package cn.com.rexen.excel.core.internal;
 
+import cn.com.rexen.core.util.ConfigUtil;
+import cn.com.rexen.excel.api.IExcelService;
+import cn.com.rexen.excel.core.ExcelServiceImpl;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.xml.sax.SAXException;
+
+import java.util.Dictionary;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by sunlf on 14-3-23.
@@ -9,7 +17,19 @@ import org.osgi.framework.BundleContext;
 public class InitActivator implements BundleActivator {
     @Override
     public void start(BundleContext bundleContext) throws Exception {
+        IExcelService ies = new ExcelServiceImpl();
+        Object wb = ies.OpenExcel("D:\\test.xls");
+        Object sheet = ies.OpenSheet(wb, "Sheet1");
 
+        int rc = ies.GetRowCount(sheet);
+        List<String> colNames = ies.GetColumnNames(sheet, 0);
+
+        Dictionary dictionary = ConfigUtil.getAllConfig("ConfigContractColumnMap");
+
+        Map rtnDic = ies.GetColumnDic(sheet, 0, dictionary);
+
+        System.out.println(rc);
+        System.out.println(colNames.toString());
     }
 
     @Override
