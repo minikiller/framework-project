2ed7a0df9d3718c0b84060e2fc9d7aa16acf274c
chenyanxu-rexen <chenyanxu@rexen.com.cn>
2015-12-26 10:30:07 +0800

    add KalixBundleActivator.java for read etc/ConfigWebContext.cfg as the parent class of the BundleActivator,so the httpService can registered a dynamic path.

---
+++ modules.core/core-api/src/main/java/cn/com/rexen/core/api/osgi/KalixBundleActivator.java
@@ -0,0 +1,15 @@
+package cn.com.rexen.core.api.osgi;
+
+import cn.com.rexen.core.util.ConfigUtil;
+import org.osgi.framework.BundleActivator;
+
+/**
+ * @author chenyanxu
+ */
+public abstract class KalixBundleActivator implements BundleActivator {
+    protected String contextPath;
+
+    public KalixBundleActivator() {
+        contextPath = (String) ConfigUtil.getConfigProp("path", "ConfigWebContext");
+    }
+}
