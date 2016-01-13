a2b959565a5c346557d0ff18adc05bdfffc885d7
zangyanming <zyanming2002@163.com>
2016-01-13 16:48:48 +0800

    修改代码生成模块

---
+++ modules.tools/code-generate/src/main/java/cn/com/rexen/bean/StudentBean.java
@@ -0,0 +1,66 @@
+package cn.com.rexen.bean;
+
+import java.util.Date;
+
+/**
+ * Created by zangyanming on 2016/1/11.
+ */
+public class StudentBean {
+
+    /**
+     * @describe 姓名
+     * @validator 不能为空
+     */
+    private String name;
+    /**
+     * @describe 是否党员
+     */
+    private boolean party;
+    /**
+     * @describe 性别
+     */
+    private String sex;
+    /**
+     * @describe 出生日期
+     */
+    private Date birthday;
+    /**
+     * @describe 年龄
+     * @validator 介于1与120之间
+     * @max 120
+     * @min 1
+     */
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
