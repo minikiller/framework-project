992bc411767a94666e6a7edfc6f6d6552217ab3a
sunlingfeng <minikiller@qq.com>
2016-02-16 14:48:09 +0800

    upgrade Activiti to 5.19.0.2 and Camel to 2.16.2

--- modules.example/example-demo/example-imp1/src/main/java/cn/com/rexen/example/imp/first/HelloFirstImpl.java
+++ modules.example/example-demo/example-imp1/src/main/java/cn/com/rexen/example/imp/first/HelloFirstImpl.java
@@ -1,10 +1,14 @@
 package cn.com.rexen.example.imp.first;
 
 import cn.com.rexen.example.api.IHelloWorld;
+import org.apache.felix.ipojo.annotations.Component;
+import org.apache.felix.ipojo.annotations.Provides;
 
 /**
  * Created by Administrator on 2015-12-30.
  */
+@Component
+@Provides
 public class HelloFirstImpl implements IHelloWorld {
     @Override
     public String getStr() {
