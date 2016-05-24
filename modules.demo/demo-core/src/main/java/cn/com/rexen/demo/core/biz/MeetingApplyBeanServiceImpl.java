16098f465d90f011c22fbd0106eab6e31e55de89
zangyanming <zyanming2002@163.com>
2016-05-24 14:56:48 +0800

    修改entity实体

--- modules.demo/demo-core/src/main/java/cn/com/rexen/demo/core/biz/MeetingApplyBeanServiceImpl.java
+++ modules.demo/demo-core/src/main/java/cn/com/rexen/demo/core/biz/MeetingApplyBeanServiceImpl.java
@@ -1,17 +1,33 @@
 package cn.com.rexen.demo.core.biz;
 
+import cn.com.rexen.bean.api.biz.IMeetingroomBeanService;
+import cn.com.rexen.core.api.persistence.JsonData;
+import cn.com.rexen.core.util.BeanUtil;
 import cn.com.rexen.demo.api.biz.IMeetingApplyBeanService;
 import cn.com.rexen.demo.api.dao.IMeetingApplyBeanDao;
 import cn.com.rexen.demo.core.Const;
 import cn.com.rexen.demo.entities.MeetingApplyBean;
 import cn.com.rexen.workflow.core.impl.WorkflowGenericBizServiceImpl;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.List;
 import java.util.Map;
 
 /**
  * @author chenyanxu
  */
 public class MeetingApplyBeanServiceImpl extends WorkflowGenericBizServiceImpl<IMeetingApplyBeanDao, MeetingApplyBean> implements IMeetingApplyBeanService {
+    private IMeetingroomBeanService meetingroomBeanService;
+
+    public IMeetingroomBeanService getMeetingroomBeanService() {
+        return meetingroomBeanService;
+    }
+
+    public void setMeetingroomBeanService(IMeetingroomBeanService meetingroomBeanService) {
+        this.meetingroomBeanService = meetingroomBeanService;
+    }
+
     @Override
     public String getProcessKeyName() {
         return Const.PROCESS_MEETING_APPLY_KEY_NAME;
@@ -23,14 +39,41 @@ public class MeetingApplyBeanServiceImpl extends WorkflowGenericBizServiceImpl<I
     }
 
     @Override
-    public void writeClaimResult(String currentTaskName, String userName, MeetingApplyBean bean) {
-        if (currentTaskName.equals("校务部文秘综合干事")) //申请部门负责人签字
-            bean.setDepUser(userName);
-        else if (currentTaskName.equals("校务部行政事务办主管")) //副校级领导审核
-            bean.setSchoolAdminUser(userName);
-        else if (currentTaskName.equals("校务部副部长")) //校务部签字
-            bean.setSchoolUser(userName);
-        else if (currentTaskName.equals("发起部门会议纪要审批")) //校务部主管领导审批（市外）
-            bean.setSchoolUser(userName);
+    public void writeClaimResult(String currentTaskId, String userName, MeetingApplyBean bean) throws NoSuchMethodException {
+        try {
+            // 将属性的首字符大写，方便构造get，set
+            String name = currentTaskId.substring(0, 1).toUpperCase() + currentTaskId.substring(1);
+            Method method = MeetingApplyBean.class.getDeclaredMethod("set" + name, String.class);
+            method.invoke(bean, userName);
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+//        if (currentTaskName.equals("校务部文秘综合干事")) //申请部门负责人签字
+//            bean.setDepUser(userName);
+//        else if (currentTaskName.equals("校务部行政事务办主管")) //副校级领导审核
+//            bean.setSchoolAdminUser(userName);
+//        else if (currentTaskName.equals("校务部副部长")) //校务部签字
+//            bean.setSchoolUser(userName);
+//        else if (currentTaskName.equals("发起部门会议纪要审批")) //校务部主管领导审批（市外）
+//            bean.setLaunchManagerUser(userName);
+    }
+
+    @Override
+    public JsonData getAllEntityByQuery(int page, int limit, String jsonStr) {
+        JsonData jsonData = new JsonData();
+        jsonData = super.getAllEntityByQuery(page, limit, jsonStr);
+
+        List beans = jsonData.getData();
+        List ids = BeanUtil.getBeanFieldValueList(beans, "meetingroomId");
+        List values = this.meetingroomBeanService.getFieldValuesByIds(ids.toArray(), "name");
+        BeanUtil.setBeanListFieldValues(beans, "meetingroomName", values);
+
+        jsonData.setTotalCount((long) beans.size());
+        jsonData.setData(beans);
+
+        return jsonData;
     }
 }
--- modules.demo/demo-web/src/main/java/cn/com/rexen/demo/web/Const.java
+++ modules.demo/demo-web/src/main/java/cn/com/rexen/demo/web/Const.java
@@ -7,6 +7,7 @@ package cn.com.rexen.demo.web;
 public class Const {
     public static final String APPLICATION_NAME = "workflow";//工作流应用
     public static final String WORKFLOW_MANAGE_MODULE_NAME = "workFlowModule";//业务流程管理模块
+    public static final String SYS_MANAGE_MODULE_NAME = "sysWorkFlowModule";//业务流程管理模块
     public static final String WORKFLOW_BIZ_MODULE_NAME = "workFlowBizModule";//业务流程模块
     public static final String MY_WORKING_MODULE_NAME = "myWorkingModule";//我的流程模块
     public static final String MY_MESSAGE_MODULE_NAME = "myMessageModule";//我的消息模块
