d7d872323d315941612cccfca33973cac09a672b
zangyanming <zyanming2002@163.com>
2016-06-02 09:23:23 +0800

    新增会议室占用情况后台服务。

--- modules.demo/demo-api/src/main/java/cn/com/rexen/demo/api/biz/IMeetingApplyBeanService.java
+++ modules.demo/demo-api/src/main/java/cn/com/rexen/demo/api/biz/IMeetingApplyBeanService.java
@@ -2,7 +2,10 @@ package cn.com.rexen.demo.api.biz;
 
 import cn.com.rexen.core.api.biz.IBizService;
 import cn.com.rexen.core.api.biz.JsonStatus;
+import cn.com.rexen.core.api.persistence.JsonData;
 import cn.com.rexen.demo.entities.MeetingApplyBean;
+
+import java.util.Date;
 
 /**
  * @author sunlf
@@ -12,4 +15,9 @@ public interface IMeetingApplyBeanService extends IBizService<MeetingApplyBean> 
     JsonStatus completeTask(String taskId, String accepted, String comment);
 
     JsonStatus checkDateTime(String jsonStr);
+
+    //会议室预约情况
+    JsonData reservation(Date date);
+
+    JsonData reservation(long roomId, Date date);
 }
